28ef07fbed34612ee38f9d22faa64e1ce00732af
Anima C13 <31348553+animaone@users.noreply.github.com>
2021-11-15 18:14:14 -0300

    Delete IFrameCallback.java

--- apk_source_files/IFrameCallback.java
+++
@@ -1,8 +0,0 @@
-package com.p004ms.ms2160.myapplication;
-
-import java.nio.ByteBuffer;
-
-/* renamed from: com.ms.ms2160.myapplication.IFrameCallback */
-public interface IFrameCallback {
-    void onFrame(ByteBuffer byteBuffer);
-}
